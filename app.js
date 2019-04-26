fc7cb7aa4c30200585599d35d6706eef6ada0b0e
rdanijel <rujevic@gmail.com>
2019-04-26 15:45:10 +0200

    added mqtt client in js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+var MQTT_CLIENT_ID = "rdanijel_aquaponic_web"+Math.floor((1 + Math.random()) * 0x10000000000).toString();
+var MQTT_CLIENT = new Paho.MQTT.Client("iot.eclipse.org", 80, "/ws", MQTT_CLIENT_ID);
+MQTT_CLIENT.connect({ onSuccess: myClientConnected });
+MQTT_CLIENT.onMessageArrived = myMessageArrived;
+
+function myClientConnected() {
+  MQTT_CLIENT.subscribe("rdanijel_aquaponic_1/test/from_nodeMcuMini");
+}
+
+function myMessageArrived(message) {
+  var messageBody = message.payloadString;
+  var messageHTML = $("<p>"+messageBody+"</p>");
+  $("#updateMe").prepend(messageHTML);
+};
+
 function myButtonWasClicked() {
   $("#updateMe").text("The text is updated!");
 }
