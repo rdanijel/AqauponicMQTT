282766ac5a87766f1850db7532fe21ef96df5bf9
rdanijel <rujevic@gmail.com>
2019-04-26 16:08:55 +0200

    same

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var MQTT_CLIENT_ID = "rdanijel_aquaponic_web"+Math.floor((1 + Math.random()) * 0x10000000000).toString();
-var MQTT_CLIENT = new Paho.MQTT.Client("wss://iot.eclipse.org", 443, "/ws", MQTT_CLIENT_ID);
-MQTT_CLIENT.connect({ onSuccess: myClientConnected });
+var MQTT_CLIENT = new Paho.MQTT.Client("iot.eclipse.org", 443, "/ws", MQTT_CLIENT_ID);
+MQTT_CLIENT.connect({ onSuccess: myClientConnected, useSSL: true });
 MQTT_CLIENT.onMessageArrived = myMessageArrived;
 
 function myClientConnected() {
